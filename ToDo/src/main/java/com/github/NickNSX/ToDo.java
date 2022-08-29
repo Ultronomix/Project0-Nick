33532b361b6da2b9dee966b60749550b9c152169
NickNSX <nickogir@gmail.com>
2022-08-28 22:05:50 -0500

    Project

--- ToDo/src/main/java/com/github/NickNSX/ToDo.java
+++ ToDo/src/main/java/com/github/NickNSX/ToDo.java
@@ -5,7 +5,5 @@ import com.github.NickNSX.common.util.AppContext;
 public class ToDo {
     public static void main(String[] args) {
         new AppContext().startApp();
-        //System.out.println("DELETE FROM tasks.user_task " +
-        //"WHERE name = \'?\'");
     }
 }
